db18e39d4863af4ea45cdc723b5663409f49e8ad
zoriana <zorianatsarenko@gmail.com>
2021-05-07 23:30:34 +0300

    get response from api

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,18 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
 
+const URL = "https://restcountries.eu/rest/v2/all";
+
 function Game() {
+  const [countries, setCountries] = useState([]);
+  const [options, setOptions] = useState([]);
+  useEffect(() => {
+    async function getData() {
+      let response = await axios.get(URL);
+      console.log(response);
+    }
+    getData();
+  });
   return (
     <div>
       <h1>Game Component</h1>
