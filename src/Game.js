56512f76bbfcd314cdc6ea1e390eba1f49e14a4a
zoriana tsarenko <zorianatsarenko@gmail.com>
2021-05-08 23:11:32 +0300

    get 4 options

--- src/Game.js
+++ src/Game.js
@@ -6,12 +6,17 @@ const URL = "https://restcountries.eu/rest/v2/all";
 function Game() {
   const [countries, setCountries] = useState([]);
   const [options, setOptions] = useState([]);
+  const [correctOption, setCorrectOption] = useState([]);
   useEffect(() => {
     fetchData();
+    getOptions();
   }, []);
   const fetchData = async () => {
     let response = await axios.get(URL);
     setCountries(response.data);
+    const correctOption = Math.floor(Math.random() * countries.length);
+
+    setCorrectOption(correctOption);
     displayData();
   };
   const displayData = () => {
@@ -20,9 +25,19 @@ function Game() {
       return <h1>country.name</h1>;
     });
   };
+  const getOptions = () => {
+    const options = [correctOption];
+
+    while (options.length < 4) {
+      let option = Math.floor(Math.random() * countries.length);
+      options.push(option);
+    }
+    setOptions(options);
+  };
   return (
     <div>
       <h1>Game Component</h1>
+      <div>sfsdf</div>
     </div>
   );
 }
